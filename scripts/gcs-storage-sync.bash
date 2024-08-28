1b17612a1e76c73b7f064b2a18ef009c5aa243b4
pjames <pjames@gmail.com>
2024-08-28 17:56:08 +0700

    Added verbose message

--- scripts/gcs-storage-sync.bash
+++ scripts/gcs-storage-sync.bash
@@ -7,6 +7,9 @@ gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS
 #SRC_DIR=/data/source
 #DST_BUCKET=dest-bucket
 
-LOG_FILE1=/tmp/${DST_BUCKET}.log
+echo "### Start syncing files..."
 
+LOG_FILE1=/tmp/${DST_BUCKET}.log
 gsutil -m rsync -r ${SRC_DIR} gs://${DST_BUCKET} 2>&1 | tee ${LOG_FILE1}
+
+echo "### Done syncing files"
