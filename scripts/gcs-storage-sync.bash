08256e469e6f91e8c18a5f9c321723a890ade97d
pjames <pjames@gmail.com>
2024-08-29 13:56:14 +0700

    Added script

--- scripts/gcs-storage-sync.bash
+++ scripts/gcs-storage-sync.bash
@@ -5,7 +5,7 @@ DATE_STAMP=$(date '+%Y-%m-%d')
 gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 
 #SRC_DIR=/data/source
-#DST_BUCKET=dest-bucket
+#DST_BUCKET=dest-bucket/xxxxxx
 
 echo "### Start syncing files..."
 
